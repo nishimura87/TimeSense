5628c612af261f20ad12c3c46a393b73f5baf4e4
nishimura87 <nishimura.k87@gmail.com>
2024-02-04 09:49:04 +0900

    環境構築

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!-- resources/views/index.blade.php -->
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>laravelView側タイトル</title>
+    @viteReactRefresh
+    @vite([
+    'resources/css/app.css',
+    'resources/ts/index.tsx',
+    ])
+</head>
+<body>
+    <div id="app"></div>
+</body>
+</html>
