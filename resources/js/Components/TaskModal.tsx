0cf1f07e20eb20bfb07b5c5cfe61229949a69c04
nishimura87 <nishimura.k87@gmail.com>
2024-06-09 09:03:11 +0900

    インデント修正

--- resources/js/Components/TaskModal.tsx
+++ resources/js/Components/TaskModal.tsx
@@ -90,88 +90,90 @@ const TaskModal: React.FC<TaskModalProps> = ({ isOpen, onClose, onSave, start, e
     })
 };
 
-  if (!isOpen) return null;
+    if (!isOpen) return null;
 
-  return (
-    <div className="fixed inset-0 z-50 overflow-auto bg-black bg-opacity-40 flex items-center justify-center" onClick={handleClose}>
-      <div className="bg-white p-5 rounded-lg shadow-lg max-w-md w-full" onClick={(e) => e.stopPropagation()}>
-        <div className='flex justify-end mb-2'>
-          <button
-            className="text-xl font-semibold"
-            onClick={onClose}
-          >
-            &times; {/* 「×」記号 */}
-          </button>
+    return (
+        <div className="fixed inset-0 z-50 overflow-auto bg-black bg-opacity-40 flex items-center justify-center" onClick={handleClose}>
+            <div className="bg-white p-5 rounded-lg shadow-lg max-w-md w-full" onClick={(e) => e.stopPropagation()}>
+                <div className='flex justify-end mb-2'>
+                    <button
+                        className="text-xl font-semibold"
+                        onClick={onClose}
+                    >
+                        &times; {/* 「×」記号 */}
+                    </button>
+                </div>
+                <div className='flex items-center mb-2'>
+                    <div className='mr-2'>
+                        <span>{formatDate(start)}</span>
+                    </div>
+                    <div className='mx-2'>
+                        <Select
+                            id="start"
+                            options={generateTimeOptions('start')}
+                            value={inputStart}
+                            onChange={(selectedOption) => setInputStart(selectedOption || { label: '', value: '' })}
+                            components={{
+                                IndicatorSeparator: () => null,
+                            }}
+                            styles={{
+                                control: (base) => ({
+                                    ...base,
+                                    "*": {
+                                        boxShadow: "none !important",
+                                    },
+                                }),
+                            }}
+                        />
+                    </div>
+                    <div>-</div>
+                    <div className='mx-2'>
+                        <Select
+                            id="end"
+                            options={generateTimeOptions('end')}
+                            value={inputEnd}
+                            onChange={(selectedOption) => setInputEnd(selectedOption || { label: '', value: '' })}
+                            components={{
+                                IndicatorSeparator: () => null,
+                            }}
+                            styles={{
+                                control: (base) => ({
+                                    ...base,
+                                    "*": {
+                                        boxShadow: "none !important",
+                                    },
+                                }),
+                            }}
+                        />
+                    </div>
+                </div>
+                <div className='relative border-b border-gray-300 mb-2'>
+                    <input
+                        type="text"
+                        id="taskName"
+                        className="w-full border-none focus:ring-transparent px-0 pb-2"
+                        placeholder="タスク名"
+                        value={taskName}
+                        onChange={(e) => setTaskName(e.target.value)}
+                    />
+                </div>
+                <div className='relative border-b border-gray-300 flex items-center mb-2'>
+                    <textarea
+                        id="taskDescription"
+                        className="w-full border-none focus:ring-transparent px-0 pb-2"
+                        placeholder="メモ"
+                        value={taskDescription}
+                        onChange={(e) => setTaskDescription(e.target.value)}
+                    ></textarea>
+                </div>
+                <div className="flex justify-end">
+                    <button className="btn bg-blue-500 hover:bg-blue-400 text-white rounded-lg px-4 py-2" onClick={handleSave}>
+                        保存
+                    </button>
+                </div>
+            </div>
         </div>
-        <div className='flex items-center mb-2'>
-          <div className='mr-2'>
-            <span>{formatDate(start)}</span>
-          </div>
-          <div className='mx-2'>
-            <Select
-              id="start"
-              options={generateTimeOptions('start')}
-              value={inputStart}
-              onChange={(selectedOption) => setInputStart(selectedOption || { label: '', value: '' })}
-              components={{
-                IndicatorSeparator: () => null,
-              }}
-              styles={{
-                control: (base) => ({
-                  ...base,
-                  "*": {
-                    boxShadow: "none !important",
-                  },
-                }),
-              }}
-            />
-          </div>
-          <div>-</div>
-          <div className='mx-2'>
-            <Select
-              id="end"
-              options={generateTimeOptions('end')}
-              value={inputEnd}
-              onChange={(selectedOption) => setInputEnd(selectedOption || { label: '', value: '' })}
-              components={{
-                IndicatorSeparator: () => null,
-              }}
-              styles={{
-                control: (base) => ({
-                  ...base,
-                  "*": {
-                    boxShadow: "none !important",
-                  },
-                }),
-              }}
-            />
-          </div>
-        </div>
-        <div className='relative border-b border-gray-300 mb-2'>
-            <input
-              type="text"
-              id="taskName"
-              className="w-full border-none focus:ring-transparent px-0 pb-2"
-              placeholder="タスク名" 
-              value={taskName}
-              onChange={(e) => setTaskName(e.target.value)}
-            />
-        </div>
-        <div className='relative border-b border-gray-300 flex items-center mb-2'>
-          <textarea
-            id="taskDescription"
-            className="w-full border-none focus:ring-transparent px-0 pb-2"
-            placeholder="メモ" 
-            value={taskDescription}
-            onChange={(e) => setTaskDescription(e.target.value)}
-          ></textarea>
-        </div>
-        <div className="flex justify-end">
-          <button className="btn bg-blue-500 hover:bg-blue-400 text-white rounded-lg px-4 py-2" onClick={handleSave}>保存</button>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default TaskModal;
